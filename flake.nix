aa16ce8a91aa5e97199ceadddd6aa4eb1152f57a
Angelo Clauin <contact@l31.dev>
2024-01-04 16:37:01 +0100

    feat(flake): add development shell for consistent tooling
    Adds a development shell configuration to ensure consistency
    in tools and versions for all users. Updates the `flake.nix`
    file to include `nodejs_21` and `corepack` packages within
    the default development shell for `x86_64-linux` system.

--- flake.nix
+++ flake.nix
@@ -6,11 +6,15 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          nodejs_21
-          corepack
-        ];
+
+      # development shell to ensure everyone 
+      # have the same tools
+      devShells.${system}.default = pkgs.mkShell { 
+        packages = with pkgs; [ 
+          nodejs_21 
+          corepack 
+        ]; 
       };
+
     };
 }
