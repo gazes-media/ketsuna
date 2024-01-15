716e17d6fdcd70c9f61d040238c2044a013a0917
Jérémy <jeremy27.clara22@gmail.com>
2024-01-15 17:49:32 +0100

    Updating to use a Boolean option for Loras Preprompt

--- src/bot/commands/ai.ts
+++ src/bot/commands/ai.ts
@@ -255,23 +255,23 @@ const commandData = new SlashCommandBuilder()
       ),
   )
   .addSubcommand((subcommand) =>
-        subcommand.setName("config")
-        .setDescription("Configure the AI")
-        .setDescriptionLocalizations({
-            fr: "Configurer l'IA",
+    subcommand.setName("config")
+      .setDescription("Configure the AI")
+      .setDescriptionLocalizations({
+        fr: "Configurer l'IA",
+      })
+      .addBooleanOption((option) =>
+        option.setName("loras_preprompt").setDescription("Whether to use Loras as preprompt").setRequired(false).setDescriptionLocalizations({
+          fr: "Utiliser Loras comme preprompt",
+        }).setNameLocalizations({
+          fr: "loras_preprompt",
         })
-        .addStringOption((option) =>
-          option.setName("loras_preprompt").setDescription("Whether to use Loras as preprompt").setRequired(false).setDescriptionLocalizations({
-            fr: "Utiliser Loras comme preprompt",
-          }).setNameLocalizations({
-            fr: "loras_preprompt",
-          }).setAutocomplete(true)
-        ).addNumberOption((option) =>
-          option.setName("cfg_scale").setDescription("How much the IA should listen").setRequired(false).setDescriptionLocalizations({
-            fr: "A quel point l'IA doit écouter",
-          }).setNameLocalizations({
-            fr: "cfg_scale",
-          })
+      ).addNumberOption((option) =>
+        option.setName("cfg_scale").setDescription("How much the IA should listen").setRequired(false).setDescriptionLocalizations({
+          fr: "A quel point l'IA doit écouter",
+        }).setNameLocalizations({
+          fr: "cfg_scale",
+        })
           .addChoices(
             {
               name: "0.10",
@@ -293,58 +293,58 @@ const commandData = new SlashCommandBuilder()
               name: "1.00",
               value: 1.00,
             })
-        )
-        .addStringOption((option) =>
-            option.setName("shared_key").setDescription("The shared key to use").setRequired(false).setDescriptionLocalizations({
-              fr: "La clé partagée à utiliser",
-            }).setNameLocalizations({
-              fr: "shared_key",
-            })
-        )
-        .addStringOption((option) =>
-            option.setName("upscaler")
-            .setDescription("The upscaller to use")
-            .setRequired(false)
-            .setDescriptionLocalizations({
-                fr: "L'upscaller à utiliser",
-            })
-            .setNameLocalizations({
-                fr: "upscaller",
-            })
-            .addChoices(
-              {
-                name: ModelGenerationInputPostProcessingTypes.RealESRGAN_x4plus,
-                value: ModelGenerationInputPostProcessingTypes.RealESRGAN_x4plus,
-              },
-              {
-                name: ModelGenerationInputPostProcessingTypes["4x_AnimeSharp"],
-                value: ModelGenerationInputPostProcessingTypes["4x_AnimeSharp"],
-              },
-              {
-                name: ModelGenerationInputPostProcessingTypes.CodeFormers,
-                value: ModelGenerationInputPostProcessingTypes.CodeFormers,
-              },
-              {
-                name: ModelGenerationInputPostProcessingTypes.GFPGAN,
-                value: ModelGenerationInputPostProcessingTypes.GFPGAN,
-              },
-              {
-                name: ModelGenerationInputPostProcessingTypes.NMKD_Siax,
-                value: ModelGenerationInputPostProcessingTypes.NMKD_Siax,
-              },
-              {
-                name: ModelGenerationInputPostProcessingTypes.RealESRGAN_x2plus,
-                value: ModelGenerationInputPostProcessingTypes.RealESRGAN_x2plus,
-              },{
-                name: ModelGenerationInputPostProcessingTypes.RealESRGAN_x4plus_anime_6B,
-                value: ModelGenerationInputPostProcessingTypes.RealESRGAN_x4plus_anime_6B,
-              },{
-                name: ModelGenerationInputPostProcessingTypes.strip_background,
-                value: ModelGenerationInputPostProcessingTypes.strip_background,
-              }
-            )
-        )
-        .addStringOption((option) =>
+      )
+      .addStringOption((option) =>
+        option.setName("shared_key").setDescription("The shared key to use").setRequired(false).setDescriptionLocalizations({
+          fr: "La clé partagée à utiliser",
+        }).setNameLocalizations({
+          fr: "shared_key",
+        })
+      )
+      .addStringOption((option) =>
+        option.setName("upscaler")
+          .setDescription("The upscaller to use")
+          .setRequired(false)
+          .setDescriptionLocalizations({
+            fr: "L'upscaller à utiliser",
+          })
+          .setNameLocalizations({
+            fr: "upscaller",
+          })
+          .addChoices(
+            {
+              name: ModelGenerationInputPostProcessingTypes.RealESRGAN_x4plus,
+              value: ModelGenerationInputPostProcessingTypes.RealESRGAN_x4plus,
+            },
+            {
+              name: ModelGenerationInputPostProcessingTypes["4x_AnimeSharp"],
+              value: ModelGenerationInputPostProcessingTypes["4x_AnimeSharp"],
+            },
+            {
+              name: ModelGenerationInputPostProcessingTypes.CodeFormers,
+              value: ModelGenerationInputPostProcessingTypes.CodeFormers,
+            },
+            {
+              name: ModelGenerationInputPostProcessingTypes.GFPGAN,
+              value: ModelGenerationInputPostProcessingTypes.GFPGAN,
+            },
+            {
+              name: ModelGenerationInputPostProcessingTypes.NMKD_Siax,
+              value: ModelGenerationInputPostProcessingTypes.NMKD_Siax,
+            },
+            {
+              name: ModelGenerationInputPostProcessingTypes.RealESRGAN_x2plus,
+              value: ModelGenerationInputPostProcessingTypes.RealESRGAN_x2plus,
+            }, {
+            name: ModelGenerationInputPostProcessingTypes.RealESRGAN_x4plus_anime_6B,
+            value: ModelGenerationInputPostProcessingTypes.RealESRGAN_x4plus_anime_6B,
+          }, {
+            name: ModelGenerationInputPostProcessingTypes.strip_background,
+            value: ModelGenerationInputPostProcessingTypes.strip_background,
+          }
+          )
+      )
+      .addStringOption((option) =>
         option
           .setName("loras")
           .setDescription("Loras to use")
@@ -399,133 +399,133 @@ const commandData = new SlashCommandBuilder()
             fr: "Activer le NSFW",
           }),
       )
-        .addStringOption((option) =>
-            option
-              .setName("model")
-              .setDescription("A style to choose for the image")
-              .setRequired(false)
-              .setDescriptionLocalizations({
-                fr: "Un style à choisir pour l'image",
-              })
-              .setNameLocalizations({
-                fr: "modèle",
-              })
-              .setAutocomplete(true),
-          )
-          .addNumberOption((option) =>
-            option
-              .setName("step")
-              .setDescription("The number of steps to perform")
-              .setRequired(false)
-              .setDescriptionLocalizations({
-                fr: "Le nombre d'étapes à effectuer",
-              })
-              .setMinValue(1)
-              .setMaxValue(500),
-          )
-          .addNumberOption((option) =>
-            option
-              .setName("width")
-              .setDescription("The width of the image")
-              .setRequired(false)
-              .setDescriptionLocalizations({
-                fr: "La largeur de l'image",
-              })
-              .setMinValue(64)
-              .setMaxValue(3072),
-          )
-          .addNumberOption((option) =>
-            option
-              .setName("height")
-              .setDescription("The height of the image")
-              .setRequired(false)
-              .setDescriptionLocalizations({
-                fr: "La hauteur de l'image",
-              })
-              .setMinValue(64)
-              .setMaxValue(3072),
-          )
-          .addStringOption((option) =>
-            option
-              .setName("sampler")
-              .setDescription("The name of the sampler")
-              .setRequired(false)
-              .setDescriptionLocalizations({
-                fr: "Le nom de l'échantillonneur",
-              })
-              .addChoices(
-                {
-                  name: ModelGenerationInputStableSamplers.DDIM,
-                  value: ModelGenerationInputStableSamplers.DDIM,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.dpmsolver,
-                  value: ModelGenerationInputStableSamplers.dpmsolver,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.PLMS,
-                  value: ModelGenerationInputStableSamplers.PLMS,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.k_dpm_2,
-                  value: ModelGenerationInputStableSamplers.k_dpm_2,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.k_dpm_2_a,
-                  value: ModelGenerationInputStableSamplers.k_dpm_2_a,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.k_dpm_adaptive,
-                  value: ModelGenerationInputStableSamplers.k_dpm_adaptive,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.k_dpm_fast,
-                  value: ModelGenerationInputStableSamplers.k_dpm_fast,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.k_dpmpp_2m,
-                  value: ModelGenerationInputStableSamplers.k_dpmpp_2m,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.k_dpmpp_2s_a,
-                  value: ModelGenerationInputStableSamplers.k_dpmpp_2s_a,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.k_euler,
-                  value: ModelGenerationInputStableSamplers.k_euler,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.k_heun,
-                  value: ModelGenerationInputStableSamplers.k_heun,
-                },
-                {
-                  name: ModelGenerationInputStableSamplers.k_lms,
-                  value: ModelGenerationInputStableSamplers.k_lms,
-                },
-              ),
-          )
-          .addNumberOption((option) =>
-            option
-              .setName("clip_skip")
-              .setDescription("The number of steps to skip")
-              .setRequired(false)
-              .setDescriptionLocalizations({
-                fr: "Le nombre de pas à ignorer",
-              })
-              .setMaxValue(12)
-              .setMinValue(1),
-          )
-          .addNumberOption((option) =>
-            option
-              .setName("numbers")
-              .setDescription("The number of images to generate")
-              .setRequired(false)
-              .setDescriptionLocalizations({
-                fr: "Le nombre d'image à générer",
-              })
-              .setMaxValue(10)
-              .setMinValue(1),
-          )
+      .addStringOption((option) =>
+        option
+          .setName("model")
+          .setDescription("A style to choose for the image")
+          .setRequired(false)
+          .setDescriptionLocalizations({
+            fr: "Un style à choisir pour l'image",
+          })
+          .setNameLocalizations({
+            fr: "modèle",
+          })
+          .setAutocomplete(true),
+      )
+      .addNumberOption((option) =>
+        option
+          .setName("step")
+          .setDescription("The number of steps to perform")
+          .setRequired(false)
+          .setDescriptionLocalizations({
+            fr: "Le nombre d'étapes à effectuer",
+          })
+          .setMinValue(1)
+          .setMaxValue(500),
+      )
+      .addNumberOption((option) =>
+        option
+          .setName("width")
+          .setDescription("The width of the image")
+          .setRequired(false)
+          .setDescriptionLocalizations({
+            fr: "La largeur de l'image",
+          })
+          .setMinValue(64)
+          .setMaxValue(3072),
+      )
+      .addNumberOption((option) =>
+        option
+          .setName("height")
+          .setDescription("The height of the image")
+          .setRequired(false)
+          .setDescriptionLocalizations({
+            fr: "La hauteur de l'image",
+          })
+          .setMinValue(64)
+          .setMaxValue(3072),
+      )
+      .addStringOption((option) =>
+        option
+          .setName("sampler")
+          .setDescription("The name of the sampler")
+          .setRequired(false)
+          .setDescriptionLocalizations({
+            fr: "Le nom de l'échantillonneur",
+          })
+          .addChoices(
+            {
+              name: ModelGenerationInputStableSamplers.DDIM,
+              value: ModelGenerationInputStableSamplers.DDIM,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.dpmsolver,
+              value: ModelGenerationInputStableSamplers.dpmsolver,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.PLMS,
+              value: ModelGenerationInputStableSamplers.PLMS,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.k_dpm_2,
+              value: ModelGenerationInputStableSamplers.k_dpm_2,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.k_dpm_2_a,
+              value: ModelGenerationInputStableSamplers.k_dpm_2_a,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.k_dpm_adaptive,
+              value: ModelGenerationInputStableSamplers.k_dpm_adaptive,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.k_dpm_fast,
+              value: ModelGenerationInputStableSamplers.k_dpm_fast,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.k_dpmpp_2m,
+              value: ModelGenerationInputStableSamplers.k_dpmpp_2m,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.k_dpmpp_2s_a,
+              value: ModelGenerationInputStableSamplers.k_dpmpp_2s_a,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.k_euler,
+              value: ModelGenerationInputStableSamplers.k_euler,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.k_heun,
+              value: ModelGenerationInputStableSamplers.k_heun,
+            },
+            {
+              name: ModelGenerationInputStableSamplers.k_lms,
+              value: ModelGenerationInputStableSamplers.k_lms,
+            },
+          ),
+      )
+      .addNumberOption((option) =>
+        option
+          .setName("clip_skip")
+          .setDescription("The number of steps to skip")
+          .setRequired(false)
+          .setDescriptionLocalizations({
+            fr: "Le nombre de pas à ignorer",
+          })
+          .setMaxValue(12)
+          .setMinValue(1),
+      )
+      .addNumberOption((option) =>
+        option
+          .setName("numbers")
+          .setDescription("The number of images to generate")
+          .setRequired(false)
+          .setDescriptionLocalizations({
+            fr: "Le nombre d'image à générer",
+          })
+          .setMaxValue(10)
+          .setMinValue(1),
+      )
   )
   .addSubcommandGroup((subcommandGroup) =>
     subcommandGroup
@@ -827,17 +827,17 @@ export class IaCommand extends CommandsBase {
 function lorasLookup(value: string, interaction: AutocompleteInteraction, client: Bot) {
   if (!isNaN(Number(value))) {
     client.getLorasModel(value).then((Loras) => {
-        interaction.respond([
-          {
-            name: Loras.name,
-            value: String(Loras.id),
-          }
-        ]).catch((_) => {});
+      interaction.respond([
+        {
+          name: Loras.name,
+          value: String(Loras.id),
+        }
+      ]).catch((_) => { });
     }).catch((err) => {
       interaction.respond([{
-        name:"No loras found",
+        name: "No loras found",
         value: "0"
-      }]).catch((_) => {});
+      }]).catch((_) => { });
     });
   } else {
     client.getLorasModels({
@@ -853,12 +853,12 @@ function lorasLookup(value: string, interaction: AutocompleteInteraction, client
             };
           }),
         )
-        .catch((_) => {});
+        .catch((_) => { });
     }).catch((err) => {
-       interaction.respond([{
-        name:"No loras found",
+      interaction.respond([{
+        name: "No loras found",
         value: "0"
-      }]).catch((_) => {});
+      }]).catch((_) => { });
     });
   }
 }
